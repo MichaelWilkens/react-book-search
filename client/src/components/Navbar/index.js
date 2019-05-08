8f60aa5dcd29250772787b8decd594130ca48da8
MichaelWilkens <mwilkens12@gmail.com>
2019-05-07 18:14:51 -0700

    link to

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Navbar extends React.Component {
     render() {
@@ -12,10 +13,10 @@ class Navbar extends React.Component {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/">Search <span className="sr-only">(current)</span></a>
+                                <a className="nav-link" Link to="/">Search <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/saved">Saved</a>
+                                <a className="nav-link" Link to="/saved">Saved</a>
                             </li>
                         </ul>
                     </div>
